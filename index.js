438b8a37ff1e0d9090cfcbc975e55f9f8d3b6f69
Thomas Fournier <thomas@artivain.com>
2022-05-15 14:44:26 -0400

    update to artibot v3

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
-// Manifest file for documentation module
+import { SlashCommandBuilder } from "@discordjs/builders";
+import Artibot, { Module, SlashCommand } from "artibot";
 
-const config = require("./config.json");
+import { createRequire } from 'module';
 
-module.exports = {
-	manifest: {
+const require = createRequire(import.meta.url);
+const { version } = require('./package.json');
 
-		manifestVersion: 1,
-		moduleVersion: "1.1.0",
-		name: "Documentation",
-		supportedLocales: "any",
-		parts: [
-			{
-				id: config.commandName,
-				type: "slashcommand",
-				path: "documentation.js"
-			}
-		]
+/**
+ * Documentation module
+ * Allows to create a custom help command from a simple config.
+ * @author GoudronViande24
+ * @license MIT
+ * @param {Artibot} artibot
+ * @returns {Module}
+ */
+export default ({ config }) => {
+	try {
+		var { name, commandName, commandDescription, argName, argDescription, pages } = config.documentation;
+		if (!name || !commandName || !commandDescription || !argName || !argDescription || !pages) throw new Error();
+		var choices = [];
+		pages.forEach(page => choices.push([page.name, page.name]));
+	} catch {
+		throw new Error("[Documentation] Config is not valid!");
 	}
-};
+
+	return new Module({
+		id: "documentation",
+		name: "Documentation",
+		version,
+		repo: "GoudronViande24/artibot-documentation",
+		langs: "any",
+		parts: [
+			new SlashCommand({
+				id: commandName,
+				data: new SlashCommandBuilder()
+					.setName(commandName)
+					.setDescription(commandDescription)
+					.addStringOption(option =>
+						option
+							.setName(argName)
+							.setDescription(argDescription)
+							.setRequired(true)
+							.addChoices(choices)
+					),
+				mainFunction: async (interaction, { createEmbed }) => {
+					const page = pages.find(page => page.name == interaction.options.getString(argName));
+
+					const embed = createEmbed()
+						.setTitle(`${name} | ${page.name}`)
+						.setDescription(page.content);
+
+					if (page.icon) embed.setThumbnail(page.icon);
+					if (page.image) embed.setImage(page.image);
+					if (page.author) embed.setAuthor(page.author);
+
+					await interaction.reply({
+						embeds: [embed],
+						ephemeral: true
+					});
+				}
+			})
+		]
+	});
+}
